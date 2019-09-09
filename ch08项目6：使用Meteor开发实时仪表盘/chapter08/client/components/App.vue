facd4d58bba7164b960bb8e1b8258e06111ff865
jyoshun <503515144@qq.com>
2019-09-09 22:02:57 +0800

    chapter08 complete

--- ch08项目6：使用Meteor开发实时仪表盘/chapter08/client/components/App.vue
+++ ch08项目6：使用Meteor开发实时仪表盘/chapter08/client/components/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <h1>Meteor</h1>
+    <nav>
+      <router-link :to="{ name: 'dashboard' }" exact>Dashboard</router-link>
+      <router-link :to="{ name: 'generate' }">Measure</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
--- ch08项目6：使用Meteor开发实时仪表盘/chapter08/client/main.js
+++ ch08项目6：使用Meteor开发实时仪表盘/chapter08/client/main.js
@@ -1,10 +1,21 @@
 import { Meteor } from 'meteor/meteor'
 import Vue from 'vue'
 import App from './components/App.vue'
+import router from './router'
+import VueMeteorTracker from 'vue-meteor-tracker'
+import 'vue-progress-path/dist/vue-progress-path.css'
+import VueProgress from 'vue-progress-path'
+
+Vue.use(VueMeteorTracker)
+
+Vue.use(VueProgress, {
+  defaultShape: 'semicircle',
+})
 
 Meteor.startup(() => {
   new Vue({
     el: '#app',
+    router,
     ...App,
   })
 })
